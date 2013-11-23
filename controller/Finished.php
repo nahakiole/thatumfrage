3584993471b22464ebe176d7dc6b4e55f442fe6c
Nahakiole <robin.glauser@gmail.com>
2013-11-23 15:34:42 +0100

    Initial import 2.

--- controller/Finished.php
+++ controller/Finished.php
@@ -1,19 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Robin
- * Date: 16.11.13
- * Time: 15:43
- */
-
-namespace Controller;
-
-
-class Finished extends Page {
-
-    function getPage()
-    {
-        $this->pageTheme = 'finished.html';
-        // TODO: Implement getPage() method.
-    }
-}
+<?php/** * Created by PhpStorm. * User: Robin * Date: 16.11.13 * Time: 15:43 */namespace Controller;class Finished extends Page {    function getPage()    {        $this->pageTheme = 'finished.html';        // TODO: Implement getPage() method.    }}
